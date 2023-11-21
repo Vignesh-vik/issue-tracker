a75f859f5441b0272bbb3763897f02699215dda5
Vignesh S <vignesh1255hg@gmail.com>
2023-11-21 20:03:57 +0530

    issue tracker added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,40 @@
 "use client";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
 import { Button, TextField } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
+interface Issueform {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<Issueform>();
+
   return (
-    <div className="p-10 max-w-xl space-y-5">
+    <form
+      className="p-10 max-w-xl space-y-5"
+      onSubmit={handleSubmit(
+        async (data) => {await axios.post("/api/issues", data)
+        router.push("/issues")
+      })}>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description"></SimpleMDE>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="kizma balls" {...field}></SimpleMDE>
+        )}
+      />
+
       <Button>Submit new Issue</Button>
-    </div>
+    </form>
   );
 };
 
